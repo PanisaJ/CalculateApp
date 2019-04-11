a017edc8b4a7ccf2cc19c88f6c15f00ac61308e0
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-04-11 15:48:32 +0700

    Clear inputs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+    num1: number;
+    num2: number;
+    result : number;
+
+    calculate(oper){
+       if(this.num1 && this.num2){
+           if(oper==1){
+               this.result = this.num1 + this.num2;
+           }else if(oper==2){
+               this.result = this.num1 - this.num2;
+           }else if(oper==3){
+               this.result = this.num1 * this.num2;
+           }else if(oper==4){
+               this.result = this.num1 / this.num2;
+           }
+       }
+    }
+
+    clear(){
+       if(this.num1 || this.num2 || this.result){
+             this.num1 = null;
+             this.num2 = null;
+             this.result = null;
+       }
+    }
+
 }
